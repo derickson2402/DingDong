ed542ea91e424dc29ec9a7fb426a52eb72bcf553
Dan Erickson <dan@danerick.com>
2023-04-17 00:40:43 -0400

    Configure cli to work from container for testing

--- src/cli/DingDongCli.py
+++ src/cli/DingDongCli.py
@@ -34,49 +34,60 @@ License
 
 #############################################################################"""
 
-from os import path, getenv
+from os import path, getenv, environ
 import requests
 import time
-import RPi.GPIO as GPIO
+# not allowed to execute RPi libary in dev container, so simulate doorbell with
+# keyboard buttons instead
+if not 'DEV_CONTAINER' in environ:
+	import RPi.GPIO as GPIO
 from pygame import mixer
 import threading
 
 # Load settings from env, as we are running in container
 try:
 	API_URL = getenv('API_URL')
+	if API_URL is None:
+		print("Make sure to specify $API_URL environment variable")
+		exit(1)
+	elif not API_URL[0:4] == 'http':
+		API_URL = 'https://' + API_URL
 	POLL_INTERVAL = int(getenv('POLL_INTERVAL', 30))
 	BELL_PIN = int(getenv('DOORBELL_PIN', 16))
-	SOUND_FILE = 'currentSound.mp3'
+	SOUND_FILE = 'CurrentSound.mp3'
 except:
-	print("FATAL: please specify $API_URL environment variable")
+	print("FATAL: could not parse env vars")
 	exit(1)
-confDict = {'volume': '50', 'currentSound': None}
+confDict = {'Volume': '50', 'CurrentSound': None}
 
-# Boot the audio device
-try:
-	mixer.init()
-except Exception as e:
-	print(e)
-	print("FATAL: could not start the audio engine")
-	exit(1)
+# Boot the audio device if we are on the Pi, audio doesnt work in container
+if not 'DEV_CONTAINER' in environ:
+	try:
+		mixer.init()
+	except Exception as e:
+		print(e)
+		print("FATAL: could not start the audio engine")
+		exit(1)
 
-def checkIn():
+def checkIn() -> None:
 	"""Ping the API for updates, download new sounds if needed"""
 	url = path.join(str(API_URL), 'config')
+	print(f'pinging {url}')
 	r = requests.get(url)
+	print(f'got status {r.status_code}')
 	if not r.status_code == 200:
 		raise ConnectionError(f'ERROR: got {r.status_code} during GET to {url}')
 	# Update our local config from response
-	confDict['volume'] = r.json()['volume']
-	if not confDict['currentSound'] == r.json()['currentSound']:
+	confDict['Volume'] = r.json()['Volume']
+	if not confDict['CurrentSound'] == r.json()['CurrentSound']:
 		if downloadSound():
 			raise ConnectionAbortedError(f'ERROR: failed to download new sound. Skipping update...')
 		else:
-			confDict['currentSound'] = r.json()['currentSound']
+			confDict['CurrentSound'] = r.json()['CurrentSound']
 	return
 
 def downloadSound() -> bool:
-	"""Download the currentSound from API and set it locally. Returns True on success"""
+	"""Download the CurrentSound from API and set it locally. Returns True on success"""
 	url = path.join(str(API_URL), 'download')
 	r = requests.get(url)
 	if not r.status_code == 200:
@@ -98,21 +109,31 @@ def threadCheckIn():
 		time.sleep(epoch + i*POLL_INTERVAL - time.time())
 		checkIn()
 
-def ringBell():
-	"""Reset sound playback and play the doorbell noise. Exits immidiately."""
-	mixer.stop()
-	sound = mixer.Sound(SOUND_FILE)
-	sound.set_volume(confDict['volume'] * 0.01)
-	sound.play()
+def ringBell() -> None:
+	"""Reset sound playback and play the doorbell noise. Exits immediately."""
+	if 'DEV_CONTAINER' in environ:
+		print(f'Debug: {confDict["CurrentSound"]} at {confDict["volume"]} volume')
+	else:
+		mixer.stop()
+		sound = mixer.Sound(SOUND_FILE)
+		sound.set_volume(confDict['Volume'] * 0.01)
+		sound.play()
+	return
 
 def threadListenBell():
 	"""Daemon listening for doorbell press, triggers the doorbell sound to
 	play"""
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(BELL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	while True:
-		GPIO.wait_for_edge(BELL_PIN, GPIO.FALLING)
-		ringBell()
+	if 'DEV_CONTAINER' in environ:
+		while True:
+			input() # halt until user hits enter
+			ringBell()
+	else:
+		# Can safely ignore errors below, since we know we are not on a Pi
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(BELL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		while True:
+			GPIO.wait_for_edge(BELL_PIN, GPIO.FALLING)
+			ringBell()
 
 if __name__ == '__main__':
 	# Get initial config then start up the daemon
